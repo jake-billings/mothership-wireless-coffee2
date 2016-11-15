c4e5504ee35858fa3009c137d9a0017b599d5cfc
jakebillings <jake@eakjb.com>
2016-11-15 16:58:14 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { LocationComponent } from './location/location.component';
+import { LocationsComponent } from './locations/locations.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { EditableTextFieldComponent } from './editable-text-field/editable-text-field.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDg6jwEhNyVHC-E_A-ecD0MLf8YNGnWnfw',
+  authDomain: 'mothership-wireless-coffee.firebaseio.com',
+  databaseURL: 'mothership-wireless-coffee.firebaseio.com',
+  storageBucket: 'mothership-wireless-coffee.firebaseio.com'
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent,
+    LocationsComponent,
+    EditableTextFieldComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
